ce313a90ad74437ca69e99960cb5aa2b2e851498
imran <31067252+ProgPassion@users.noreply.github.com>
2023-05-30 16:56:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-let dtObj = [{
-    nums: [3,2,1,0,4],
-    numsOfJumps: 4,
-    currentIndex: 0,
-    jumps: 1,
-}];
+/*
+ * URL: https://leetcode.com/problems/jump-game/description/
+ */
 
-let finalObj = JSON.parse(JSON.stringify(dtObj[dtObj.length - 1]));
-finalObj.nums[finalObj.currentIndex] = finalObj.nums[finalObj.currentIndex] - finalObj.jumps;
-finalObj.currentIndex += finalObj.jumps;
-finalObj.numsOfJumps -= finalObj.jumps;
+var canJump = function(nums) {
+    
+    let subArrays = [];
 
-// Make some checks if we can reach the last index and proceed with code
+    if(nums.length == 1 && nums[0] == 0) return true;
+    if(nums.length > 1 && nums[0] == 0)  return false;
 
-dtObj.push(finalObj);
+    while(nums.includes(0)) {
+        let indexOfZero = nums.indexOf(0);
+        let tmpNums = nums.slice(0, indexOfZero + 1);
+        nums = nums.slice(indexOfZero + 1);
+        if(tmpNums.length > 1) {
+            subArrays.push(tmpNums);
+        }
+        else {
+            subArrays[subArrays.length - 1].push(tmpNums[0]);
+        }
+    }
+
+    if(subArrays.length == 0)
+        return true;
+
+    function canJumpFn(subArr, lastSubArray) {
+        for(let i = 0; subArr[i] > 0; i++) {
+            let currentElem = subArr[i];
+            if(lastSubArray && currentElem >= subArr.length - 1 - i)
+                return true;
+            else if(!lastSubArray && currentElem > subArr.length - 1 - i)
+                return true;
+        }
+    
+        return false;
+    }
+    
+    for(var i = 0; i < subArrays.length; i++) {
+        if(!canJumpFn(subArrays[i], (i == subArrays.length - 1 && nums.length == 0))) {
+            if(subArrays.length == 1 || i == 0)
+                return false;
+            else {
+                let tmpSubArray = subArrays.splice(i, 1);
+                subArrays[i - 1] = subArrays[i - 1].concat(...tmpSubArray);
+                i -= 2;
+            }
+        }
+    }    
+    
+    return true;
+};
